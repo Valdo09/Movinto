3ae0d3d89b877f8aa2aee0e434811c8400dd5645
Oswaldo Camille <grimaudoswaldocamille@gmail.com>
2022-10-06 03:31:43 +0200

    setup and auth migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Role;
 
 class User extends Authenticatable
 {
@@ -18,10 +19,18 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
+        'phone_number',
+        'sex',
         'password',
+        'role_id'
     ];
+    public function role()
+    {
+        return $this->hasOne(Role::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
